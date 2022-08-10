b58bc373c09c14fbbb2a4a909fe778069aa87652
gulenc06 <108074625+gulenc06@users.noreply.github.com>
2022-08-10 13:09:14 +0300

    Update isim.java

--- java/isim.java
+++ java/isim.java
@@ -1,6 +1,7 @@
 public class Main
 {
 	public static void main(String[] args) {
+		
 		java.util.Scanner input = new java.util.Scanner (System.in);
 		java.util.Scanner kb = new java.util.Scanner (System.in);
 		String isim, soyisim;
@@ -13,8 +14,8 @@ public class Main
 		yas = kb.nextInt();
 		System.out.println("İsmin :"+isim);
 		System.out.println("Soyismin:"+soyisim);
-		if (yas==31)
-		System.out.println("31ci seni");
+		if (yas==16)
+		System.out.println("yaşın=yaşım");
 		else 
 		System.out.println("yaşın:"+yas);
 		
